4ab2ccaa774e4e4f17c329f00aec4b02c8b91fdd
Shivam Yadav <shivamptu4566@gmail.com>
2025-08-31 20:55:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://ecommerce-frontend-lgav.onrender.com" , "http://localhost:5174"],
+ origin:["https://ecommerce-frontend-lgav.onrender.com" , "https://ecommerce-admin-layr.onrender.com"],
  credentials:true
 }))
 
